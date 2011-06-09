ef6c2d79c42586401b8d27cc076a46ff45f27ddb
Sean Clark Hess <seanhess@gmail.com>
2011-06-08 20:13:58 -0600

    Initial Commit

---
+++ HeliumTests/HeliumTests.h
@@ -0,0 +1,13 @@
+//
+//  HeliumTests.h
+//  HeliumTests
+//
+//  Created by Sean Hess on 6/8/11.
+//  Copyright 2011 I.TV. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HeliumTests : SenTestCase
+
+@end
---
+++ HeliumTests/HeliumTests.m
@@ -0,0 +1,32 @@
+//
+//  HeliumTests.m
+//  HeliumTests
+//
+//  Created by Sean Hess on 6/8/11.
+//  Copyright 2011 I.TV. All rights reserved.
+//
+
+#import "HeliumTests.h"
+
+@implementation HeliumTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in HeliumTests");
+}
+
+@end

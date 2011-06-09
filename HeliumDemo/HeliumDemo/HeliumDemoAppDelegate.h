30f1b922dc7a3cc99ea9e6417ff02e9e2b995e24
Sean Clark Hess <seanhess@gmail.com>
2011-06-08 20:23:56 -0600

    Initial commit

---
+++ HeliumDemo/HeliumDemo/HeliumDemoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HeliumDemoAppDelegate.h
+//  HeliumDemo
+//
+//  Created by Sean Hess on 6/8/11.
+//  Copyright 2011 I.TV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HeliumDemoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HeliumDemo/HeliumDemo/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  HeliumDemo
+//
+//  Created by Sean Hess on 6/8/11.
+//  Copyright 2011 I.TV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HeliumDemoAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    int retVal = 0;
+    @autoreleasepool {
+        retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([HeliumDemoAppDelegate class]));
+    }
+    return retVal;
+}

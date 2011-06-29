f5c7fe2f3474df329da18ed53b34135787c6c1da
Sean Clark Hess <seanhess@gmail.com>
2011-06-29 16:31:01 -0600

    Added server

---
+++ Demo/server/app.js
@@ -0,0 +1,23 @@
+
+
+var express = require('express')
+var path = require('path')
+var hbs = require('hbs')
+
+var app = express.createServer()
+
+app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "hbs");
+app.set('view options', {layout: false}); 
+app.register(".hml", hbs);   
+
+app.get("*", function(req, res, next) {
+    res.contentType("text/xml")
+    next()
+})
+
+app.get('/main', function(req, res) {
+    res.render("main.hml")
+})
+
+app.listen(3021)
